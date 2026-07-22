6f545d02686f7a47ba37fd69d6c258bde2e1aa9e
Youssef5107 <ybelalmarzouk@gmail.com>
2026-07-22 23:12:05 +0300

    fixing some styles issues

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,31 @@ import Shop from "./pages/ShopPage/Shop";
 import Cart from "./pages/CartPage/Cart";
 import Profile from "./pages/ProfilePage/Profile";
 import Checkout from "./pages/CheckoutPage/Checkout";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
 import CeramicsCollection from "./pages/ceramicsCollectionPage/ceramicsCollection";
 import LivingRoomCollection from "./pages/livingRoomCollectionPage/LivingRoomCollection";
 import DiningCollection from "./pages/diningCollectionPage/DiningCollection";
 import BedroomCollection from "./pages/bedroomCollectionPage/BedroomCollection";
 import SavedItems from "./pages/savedItemsPage/SavedItems";
+import AccountSettings from "./pages/accountSettingsPage/AccountSettings";
+import OurStory from "./pages/ourStoryPage/OurStory";
 
 function App() {
+  const location = useLocation();
+  const pathName = location.pathname;
   return (
     <>
-      <Header />
-      <main className="pb-32">
+      {pathName == "/profile/account-settings" ||
+      pathName == "/profile/saved-items" ? null : (
+        <Header />
+      )}
+      <main
+        className={`${pathName == "/home/our-story" || pathName == "/profile/saved-items" || pathName == "/profile/account-settings" ? "pb-20" : "pb-32"}`}
+      >
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/home/our-story" element={<OurStory />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/shop/ceramics" element={<CeramicsCollection />} />
           <Route path="/shop/living-rooms" element={<LivingRoomCollection />} />
@@ -31,6 +41,10 @@ function App() {
           <Route path="cart/checkout" element={<Checkout />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/profile/saved-items" element={<SavedItems />} />
+          <Route
+            path="/profile/account-settings"
+            element={<AccountSettings />}
+          />
         </Routes>
       </main>
       <BottomNavBar />
